39ce5c7d586823a88207a573eb694c47e7da704b
Karol Stefanides <avirow.games.pl@gmail.com>
2020-11-16 17:12:14 +0100

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator();
+        int result2 = calculator.add(5, 3);
+
+        if (result2 == 8)
+        {
+            System.out.println("Test OK");
+        }
+        else
+        {
+            System.out.println("Error pierwszego testu jednostkowego");
+        }
+
+        System.out.println("Test - drugi test jednostkowy:");
+        int result3 = calculator.substract(5, 3);
+
+        if (result3 == 2)
+        {
+            System.out.println("Test OK");
+        }
+        else
+        {
+            System.out.println("Error pierwszego testu jednostkowego");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+
+    public int substract(int firstNumber, int secondNumber)
+    {
+        return firstNumber - secondNumber;
+    }
+}
