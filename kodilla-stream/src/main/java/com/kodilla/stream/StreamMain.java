3c439cb3a4cfeb063a7795bc2c84ba4c4d1cb660
Karol Stefanides <avirow.games.pl@gmail.com>
2020-11-23 18:02:46 +0100

    Zadanie 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Karol", text -> text.toLowerCase());
+        poemBeautifier.beautify("Karol", text -> text.toUpperCase());
+        poemBeautifier.beautify("Karol", text -> text+" Stefanides");
+        poemBeautifier.beautify("Karol", text -> text+"( ͡° ͜ʖ ͡°)");
+        poemBeautifier.beautify("Karol", text -> "/"+text+"/");
+    }
+}
