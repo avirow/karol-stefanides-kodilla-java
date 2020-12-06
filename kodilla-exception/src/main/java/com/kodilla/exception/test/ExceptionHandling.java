00f61816fcfdfba7361f8b38a689c027de6859df
Karol Stefanides <avirow.games.pl@gmail.com>
2020-12-06 12:02:20 +0100

    8.3 Poprawki

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,12 +1,14 @@
 package com.kodilla.exception.test;
 
+import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+
 public class ExceptionHandling {
 
     SecondChallenge secondChallenge = new SecondChallenge();
     String result;
 
-    public void handling(double x, double y) throws SecondChallengeExeption
-    {
+    public void handling(double x, double y) throws SecondChallengeExeption {
         try {
             result = secondChallenge.probablyIWillThrowException(x, y);
             System.out.println(result);
@@ -17,5 +19,24 @@ public class ExceptionHandling {
             throw new SecondChallengeExeption();
         }
     }
+
+    public static void main(String[] args)  {
+
+        String result;
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            result = secondChallenge.probablyIWillThrowException(3, 1.5);
+            System.out.println(result);
+        }
+        catch (Exception e) {
+            System.out.println("Error:" + e);
+            //throw new SecondChallengeExeption();
+        }
+        finally {
+            System.out.println("Done !");
+        }
+    }
+
 
 }
