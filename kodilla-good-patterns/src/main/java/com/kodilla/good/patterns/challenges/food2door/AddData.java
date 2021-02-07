f0796b348e96ffa0623cc6a44590a80c5c1888ba
Karol Stefanides <avirow.games.pl@gmail.com>
2021-02-07 16:37:46 +0100

    Zadanie 13.4 Poprawa

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/AddData.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/AddData.java
@@ -7,30 +7,67 @@ public class AddData {
 
         ProviderProcess producerHealthyShop = new HealthyShop("HealthyShop",
                 "Warszawa ul. Warty 43");
+        ProviderProcess producerGlutenFreeShop = new GlutenFreeShop("GlutenFreeShop",
+                "Gdynia ul. Lakoniczna 4");
+        ProviderProcess producerFastFoodShop = new FastFoodShop("FastFoodShop",
+                "Katowice ul. Spokojna 15");
 
         Product product = new Product("Mleko", 2.49, 50);
+        Product product1 = new Product("Ser owczy", 1.49, 85);
         Product product2 = new Product("Ser pleśniowy", 2.49, 35);
         Product product3 = new Product("Masło", 6.99, 80);
-        Product product4 = new Product("Kefir", 1.99, 50);
+        Product product4 = new Product("Mleko bez laktozy", 4.99, 250);
+        Product product5 = new Product("Kefir", 1.99, 50);
+        Product product6 = new Product("Maślanka", 2.50, 750);
+        Product product7 = new Product("Oscypek", 4.99, 150);
+        Product product8 = new Product("Masło osełkowe", 14.99, 50);
+        Product product9 = new Product("Kefir wiejski", 3.99, 100);
+        Product product10 = new Product("Serek wiejski", 64.99, 40);
 
 
+        RegisterProduct productsOfFastFoodShop = new RegisterProduct();
         RegisterProduct productsOfHealthyShopProducer = new RegisterProduct();
+        RegisterProduct productsOfGlutenFreeShopProducer = new RegisterProduct();
 
-        productsOfHealthyShopProducer.addNewProduct(product);
-        productsOfHealthyShopProducer.addNewProduct(product2);
-        productsOfHealthyShopProducer.addNewProduct(product3);
+        productsOfFastFoodShop.addNewProduct(product);
+        productsOfFastFoodShop.addNewProduct(product1);
+        productsOfFastFoodShop.addNewProduct(product2);
+        productsOfFastFoodShop.addNewProduct(product3);
+        productsOfFastFoodShop.addNewProduct(product4);
+
+        productsOfHealthyShopProducer.addNewProduct(product5);
+        productsOfHealthyShopProducer.addNewProduct(product6);
+        productsOfHealthyShopProducer.addNewProduct(product7);
+
+        productsOfGlutenFreeShopProducer.addNewProduct(product8);
+        productsOfGlutenFreeShopProducer.addNewProduct(product9);
+        productsOfGlutenFreeShopProducer.addNewProduct(product10);
 
 
         RegisterProducers registerProducers = new RegisterProducers();
 
+        registerProducers.addNewProducer(producerFastFoodShop, productsOfFastFoodShop);
         registerProducers.addNewProducer(producerHealthyShop, productsOfHealthyShopProducer);
+        registerProducers.addNewProducer(producerGlutenFreeShop, productsOfGlutenFreeShopProducer);
 
         Order order = new Order(new User("Karol", "Stefanides",
                  "Katowice ul. Cicha 8", "email@example.pl")
                 , LocalDate.of(2020, 10, 15), product, 2);
+        Order order2 = new Order(new User("Karol", "Stefanides",
+                "Katowice ul. Cicha 8", "email@example.pl")
+                , LocalDate.of(2020, 10, 15), product4, 200);
+        Order order3 = new Order(new User("Karol", "Stefanides",
+                "Katowice ul. Cicha 8", "email@example.pl")
+                , LocalDate.of(2020, 10, 15), product9, 20);
+        Order order4 = new Order(new User("Karol", "Stefanides",
+                "Katowice ul. Cicha 8", "email@example.pl")
+                , LocalDate.of(2020, 10, 15), product6, 10);
 
         Processor processor = new Processor(new MailInformation(), registerProducers);
         processor.process(order);
+        processor.process(order2);
+        processor.process(order3);
+        processor.process(order4);
 
     }
 }
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/FastFoodShop.java
@@ -0,0 +1,31 @@
+package com.kodilla.good.patterns.challenges.food2door;
+
+public class FastFoodShop  implements ProviderProcess {
+    private String name;
+    private String residenceAddress;
+
+    public FastFoodShop(String name, String residenceAddress) {
+        this.name = name;
+        this.residenceAddress = residenceAddress;
+    }
+
+        @Override
+        public void process(Product product, int volumeOfPurchase) {
+            System.out.print("Towar: " + product.getName() + " jest przygotowywany do wysyłki");
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getResidenceAddress() {
+            return residenceAddress;
+        }
+
+        @Override
+        public String toString() {
+            return "Nazwa producenta " + name +
+                    " siedziba " + residenceAddress;
+        }
+
+}
