523688f7bc309d70c694830ced98d05e50647462
Karol Stefanides <avirow.games.pl@gmail.com>
2021-02-21 11:44:41 +0100

    Zadanie 15.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew(){
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Lettuce")
+                .bun("Normal bun")
+                .burgers("Twice")
+                .sauce("Garlic sauce")
+                .ingredient("Tomato")
+                .ingredient("Cheese")
+                .ingredient("Bakon")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(4, howManyIngredients);
+    }
+}
